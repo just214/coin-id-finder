a14747dbf8f27a863ecedf7302703f2c2815536a
gojutin <gojutin@gmail.com>
2021-08-02 13:25:03 -0400

    seo: fixed links to image

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,7 +11,7 @@ const defaultSEO = {
   pageTitle: "Coin ID Finder",
   pageDescription:
     "A handy little tool to find CoinMarketCap and CoinGecko coin IDs for working with their respective APIs.",
-  pageImage: "/coin-id-finder-logo.svg",
+  pageImage: "coin-id-finder-logo.svg",
   pageUrl: "https://www.coin-id-finder.vercel.app/",
 };
 
@@ -28,8 +28,18 @@ export const Layout = (props: LayoutProps) => {
       <Head>
         <title>{pageTitle}</title>
         <meta name="description" content={pageDescription} />
-        <link rel="icon" type="image/png" href={pageImage} sizes="32x32" />
-        <link rel="icon" type="image/png" href={pageImage} sizes="16x16" />
+        <link
+          rel="icon"
+          type="image/png"
+          href={pageUrl + pageImage}
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href={pageUrl + pageImage}
+          sizes="16x16"
+        />
         <meta name="application-name" content={pageTitle} />
         <meta name="title" content={pageTitle} />
 
@@ -37,13 +47,13 @@ export const Layout = (props: LayoutProps) => {
         <meta property="og:url" content={pageUrl} />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={pageDescription} />
-        <meta property="og:image" content={pageImage} />
+        <meta property="og:image" content={pageUrl + pageImage} />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content={pageUrl} />
         <meta property="twitter:title" content={pageTitle} />
         <meta property="twitter:description" content={pageDescription} />
-        <meta property="twitter:image" content={pageImage}></meta>
+        <meta property="twitter:image" content={pageUrl + pageImage}></meta>
       </Head>
 
       <main className="container mx-auto px-2 md:px-32 lg:px-64 xl:px-96 mb-12 flex-1">
